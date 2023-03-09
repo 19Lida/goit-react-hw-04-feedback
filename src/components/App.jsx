b647deff1ceab57fe19aa980526f6e61f6548f5a
19Lida <lida19melnyk@gmail.com>
2023-03-09 22:28:00 +0200

    зробила рефакторинг відгуків на хуках

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
+import { useState } from 'react';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Notification } from './Notification/Notification';
 export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedbackOptions = { good, neutral, bad };
+  const total = good + neutral + bad;
+
+  const leaveFeedback = feedback => {
+    switch (feedback) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        return;
+    }
+  };
+  const countPositivePercent = () => {
+    if (!total) {
+      return 0;
+    }
+    const result = ((good / total) * 100).toFixed(1);
+    return Number(result);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(feedbackOptions)}
+          onLeaveFeedback={leaveFeedback}
+        ></FeedbackOptions>
+      </Section>
+      {total !== 0 && (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={countPositivePercent()}
+          />
+        </Section>
+      )}
+      {total === 0 && <Notification message="There is no feedback" />}
+    </>
   );
 };
